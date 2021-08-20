bf68f8f6d7cf70a76bbbee8973954578b543acac
kanamaka <ec2-user@ip-172-31-74-202.ec2.internal>
2021-08-20 10:44:22 +0000

    load

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'cb9d979d5436a6caaf3cb8b822a91628133b25b2e5bddb0c43e6990d88d1703b55ac00b1679e7d3d0533c31fcc6b025a4074d58ae67450bb5b8644cee3e63b61'
